18f9eba9d54469a58a96cfc2d28a01aac3fc0dd6
張竣凱 <kevin91177@gmail.com>
2018-10-04 20:23:47 +0800

    白癡老鼠亂打東西

--- src/main.cpp
+++ src/main.cpp
@@ -7,14 +7,14 @@ int main() {
     printf("Rule Breaker!!!\n");
     printf("5 + 2 = %d\n", addNumber(5, 2));
     printf("%d\n", getTriangleType(3, 4, 5));
-<<<<<<< HEAD
+
     printf("4 * 5 = %d\n", times(4, 5));
 
     printf("Hello, world\n");
 
     printf("a-no-ne a-no-ne\n");
     printf("MDFK\n");
-=======
+
     printf("%d\n",multiply(4, 4));
     printf("Hello, world\n");
 
@@ -28,7 +28,6 @@ int main() {
     printf("MDFK\n");
 
 
->>>>>>> master
     return 0;
 }
 #endif // TEST
--- src/main.h
+++ src/main.h
@@ -3,10 +3,9 @@
 
 int addNumber(int a, int b);
 int getTriangleType(int a, int b, int c);
-<<<<<<< HEAD
 int times(int a, int b);
-=======
+
 int multiply(int a, int b);
 
->>>>>>> master
+
 #endif // MAIN_H_INCLUDED
